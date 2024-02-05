da9d98ee5ec602ec3855bed380e44723957517e3
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-05 21:40:18 +0530

    tried bytton

--- src/first.java
+++ src/first.java
@@ -3,4 +3,6 @@ import javax.swing.*;
 public class first {
     private JPanel panel1;
     private JTextField helloTextField;
+    private JPasswordField passwordField1;
+    private JButton button1;
 }
