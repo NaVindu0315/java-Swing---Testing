b3fd415d250aea7caa871a3b217e6f89ea51dc26
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-06 18:56:40 +0530

    jframe and txtfield

--- src/Messages.java
+++ src/Messages.java
@@ -5,6 +5,10 @@ public class Messages {
     JFrame frm;
     Messages()
     {
+    frm = new JFrame("NaVindu LakShan");
+    final JTextField txtfield = new JTextField();
+    txtfield.setBounds(50,50,150,20);
+
 
     }
 }
