7ddf226ffb8f9860fe631f3300a83fc7f131c014
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-06 19:44:07 +0530

    added butn

--- src/Messages.java
+++ src/Messages.java
@@ -8,6 +8,9 @@ public class Messages {
     frm = new JFrame("NaVindu LakShan");
     final JTextField txtfield = new JTextField();
     txtfield.setBounds(50,50,150,20);
+    JButton btn1 = new JButton("Click");
+    btn1.setBounds(50,100,95,30);
+
 
 
     }
