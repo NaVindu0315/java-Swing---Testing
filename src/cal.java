e0dc89cc4f8da214420d4b02f6d6a52b67f283d7
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-07 22:14:00 +0530

    label location chgned

--- src/cal.java
+++ src/cal.java
@@ -1,44 +1,17 @@
 import javax.swing.*;
 
 public class cal {
-    JFrame frm;
-    //lables
-    JLabel lblnum1;
 
-
-    ///txt fields
-    JTextField txt1;
-
-    ///buttons
-
-    cal()
     {
-        frm = new JFrame("Calculator");
-        JPanel panel = new JPanel();
-        frm.setSize(500,500);
-
-        lblnum1 = new JLabel("Number 1");
-       // lblnum1.setBounds(20,40,300,100);
-        lblnum1.setLocation(30,40);
-
-    /*
-    *
-    * JPanel panel = new JPanel();
-panel.setLayout(null);
-
-lblnum1.setLocation(x, y);
-panel.add(lblnum1);
-    * */
-
-
-
-
-
-        //display
-        panel.setLayout(null);
-        panel.add(lblnum1);
-        frm.add(lblnum1);
-        frm.setVisible(true);
-
+        JFrame f= new JFrame("Label Example");
+        JLabel l1,l2;
+        l1=new JLabel("First Label.");
+        l1.setBounds(50,50, 100,30);
+        l2=new JLabel("Second Label.");
+        l2.setBounds(50,100, 100,30);
+        f.add(l1); f.add(l2);
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
