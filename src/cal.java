1dca414adab18d4f30dfb413ba8d0e9f95ec933b
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-07 23:01:15 +0530

    chgned some and add ed action perfomed

--- src/cal.java
+++ src/cal.java
@@ -1,48 +1,56 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class cal implements ActionListener {
 
-    {
+
         //initializing
         JFrame f= new JFrame("Calculator");
         JLabel l1,l2;
         JTextField num1,num2;
         JButton btnadd,btnsub;
 
+        cal()
+        {
+            //first label
+            l1=new JLabel("Number 1.");
+            l1.setBounds(50,50, 100,30);
+            //firs textfied
+            num1 = new JTextField();
+            num1.setBounds(120,50, 100,30);
+            num1.addActionListener(this);
 
-        //first label
-        l1=new JLabel("Number 1.");
-        l1.setBounds(50,50, 100,30);
-        //firs textfied
-        num1 = new JTextField();
-        num1.setBounds(120,50, 100,30);
-        num1.addActionListener(this);
+            //label2
+            l2=new JLabel("Number 2.");
+            l2.setBounds(50,100, 100,30);
+            //second textfied
+            num2 = new JTextField();
+            num2.setBounds(120,100, 100,30);
+            num2.addActionListener(this);
+            //buttons
+            btnadd = new JButton("Add");
+            btnadd.setBounds(50,150,60,20);
 
-        //label2
-        l2=new JLabel("Number 2.");
-        l2.setBounds(50,100, 100,30);
-        //second textfied
-        num2 = new JTextField();
-        num2.setBounds(120,100, 100,30);
-        num2.addActionListener(this);
-        //buttons
-        btnadd = new JButton("Add");
-        btnadd.setBounds(50,150,60,20);
-
-        btnsub = new JButton("subtract");
-        btnsub.setBounds(130,150,80,20);
+            btnsub = new JButton("subtract");
+            btnsub.setBounds(130,150,80,20);
 
 
-       //addings
-        f.add(l1); f.add(l2);
-        f.add(num1); f.add(num2);
-        f.add(btnadd);
-        f.add(btnsub);
+            //addings
+            f.add(l1); f.add(l2);
+            f.add(num1); f.add(num2);
+            f.add(btnadd);
+            f.add(btnsub);
 
-        //fram size
-        f.setSize(300,300);
-        f.setLayout(null);
-        f.setVisible(true);
+            //fram size
+            f.setSize(300,300);
+            f.setLayout(null);
+            f.setVisible(true);
+        }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
