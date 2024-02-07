2dad1110931bb39cf96b546156219fee2cfb23fa
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-07 21:03:47 +0530

    created class and jframe

--- src/cal.java
+++ src/cal.java
@@ -1,6 +1,18 @@
+import javax.swing.*;
+
 public class cal {
+    JFrame frm;
+
     cal()
     {
+        frm = new JFrame("Calculator");
+
+
+
+
+
+        //display
+        frm.setVisible(true);
 
     }
 }
