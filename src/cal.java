9f12e799890f2a8cc58c62c03875888f041ac459
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-07 22:00:07 +0530

    chgned didint work

--- src/cal.java
+++ src/cal.java
@@ -14,19 +14,29 @@ public class cal {
     cal()
     {
         frm = new JFrame("Calculator");
+        JPanel panel = new JPanel();
         frm.setSize(500,500);
 
         lblnum1 = new JLabel("Number 1");
-        lblnum1.setBounds(20,40,300,100);
+       // lblnum1.setBounds(20,40,300,100);
         lblnum1.setLocation(30,40);
 
+    /*
+    *
+    * JPanel panel = new JPanel();
+panel.setLayout(null);
 
+lblnum1.setLocation(x, y);
+panel.add(lblnum1);
+    * */
 
 
 
 
 
         //display
+        panel.setLayout(null);
+        panel.add(lblnum1);
         frm.add(lblnum1);
         frm.setVisible(true);
 
