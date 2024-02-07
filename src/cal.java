acf4d2b68786e8d68e1b1f1ed4bcad686d6e75df
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-02-07 21:14:06 +0530

    added label and filed

--- src/cal.java
+++ src/cal.java
@@ -2,11 +2,21 @@ import javax.swing.*;
 
 public class cal {
     JFrame frm;
+    //lables
+    JLabel lblnum1;
+
+
+    ///txt fields
+    JTextField txt1;
+
+    ///buttons
 
     cal()
     {
         frm = new JFrame("Calculator");
         frm.setSize(500,500);
+
+
 
 
 
