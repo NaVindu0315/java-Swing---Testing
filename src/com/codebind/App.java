c9a1bca3293821ee9909651fefaa94c97f746470
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-07 22:24:22 +0530

    added new function

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,8 +1,19 @@
 package com.codebind;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App {
     private JButton ButtonMsg;
     private JPanel PanalMain;
+
+    public App() {
+        ButtonMsg.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
