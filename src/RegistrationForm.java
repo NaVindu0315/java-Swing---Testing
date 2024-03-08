6aca36ae65f1d6196e59e1df7cb60bad97f074c8
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-08 18:43:56 +0530

    shit didnt work either

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class RegistrationForm {
+    private JLabel label1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
