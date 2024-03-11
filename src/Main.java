2c8bdb8b0f098f1711901646d88c2e58081c34ff
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-11 22:09:16 +0530

    first jframe

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
         JFrame frame = new JFrame();
         //frame assigned name
+        //title
+        frame.setTitle("title here");
+        //to close the program on close button
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //defining the size of the frame
         frame.setSize(420,420);
         frame.setVisible(true); //to make this visiblew
     }
