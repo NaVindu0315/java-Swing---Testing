e9c4c1b6bdbcc35c820566e7d3ea893d097fa05d
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-11 22:06:09 +0530

    first jframe

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
+        //frame assigned name
+        frame.setSize(420,420);
         frame.setVisible(true); //to make this visiblew
     }
 }
