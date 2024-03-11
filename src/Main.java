4262c8248b6bc69924a660212115d36ee3d38e3c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-11 22:14:06 +0530

    jframe layout

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         frame.setTitle("title here");
         //to close the program on close button
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false); //prevent frame from begin resized
         //defining the size of the frame
         frame.setSize(420,420);
         frame.setVisible(true); //to make this visiblew
