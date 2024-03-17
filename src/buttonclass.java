f79a13d7ea822ad22d633f1c620097394cef3bd7
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-17 23:37:07 +0530

    new button class

--- src/buttonclass.java
+++ src/buttonclass.java
@@ -6,6 +6,8 @@ public class buttonclass extends JFrame {
     {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
 
     }
 }
