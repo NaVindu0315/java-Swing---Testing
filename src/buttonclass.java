2fa9f1113525f0f04391b7c421e34a6af4020f5c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-18 10:16:01 +0530

    button added

--- src/buttonclass.java
+++ src/buttonclass.java
@@ -4,10 +4,16 @@ public class buttonclass extends JFrame {
 
     buttonclass()
     {
+        JButton button = new JButton();
+        button.setText("Payya");
+        button.setBounds(200,100,100,50);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500,500);
         this.setVisible(true);
+
+        this.add(button);
 
     }
 }
