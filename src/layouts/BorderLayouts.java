895db6b6142fba7b85a5264113242ef8f49e9d58
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-18 19:48:08 +0530

    colors to panel

--- src/layouts/BorderLayouts.java
+++ src/layouts/BorderLayouts.java
@@ -1,6 +1,7 @@
 package layouts;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BorderLayouts extends JFrame {
 
@@ -17,5 +18,12 @@ public class BorderLayouts extends JFrame {
         JPanel panel3 = new JPanel();
         JPanel panel4 = new JPanel();
         JPanel panel5 = new JPanel();
+
+        panel1.setBackground(Color.RED);
+        panel2.setBackground(Color.green);
+        panel3.setBackground(Color.yellow);
+        panel4.setBackground(Color.magenta);
+        panel5.setBackground(Color.blue);
+
     }
 }
