a88ad9724b7ecd5478f4a5b627c46f9cf9cb3d7a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-20 18:58:41 +0530

    input dialogue

--- src/panes/OptionPanes.java
+++ src/panes/OptionPanes.java
@@ -7,9 +7,13 @@ public class OptionPanes extends JFrame{
 
     public OptionPanes(){
         //JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.PLAIN_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.INFORMATION_MESSAGE);
+       // JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.INFORMATION_MESSAGE);
         //JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.QUESTION_MESSAGE);
      //   JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.WARNING_MESSAGE);
      //   JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.ERROR_MESSAGE);
+        //JOptionPane.showMessageDialog(null,"Hi hutto","paka",JOptionPane.YES_NO_CANCEL_OPTION);
+
+     //  String name = JOptionPane.showInputDialog("What is your name");
+      // System.out.println(name);
     }
 }
