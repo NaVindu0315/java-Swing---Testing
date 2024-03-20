2c118b4a8ac7ac4146b6a0aff323d6261b1da083
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-20 20:16:51 +0530

    textfields

--- src/components/TextFields.java
+++ src/components/TextFields.java
@@ -6,10 +6,28 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TextFields extends JFrame implements ActionListener {
+    JTextField textField = new JTextField();
+    JButton jbtn = new JButton("Submit");
 
     public TextFields(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
+
+
+        textField.setPreferredSize(new Dimension(250,40));
+        textField.setFont(new Font("Consolas",Font.PLAIN,35));
+        textField.setForeground(Color.GREEN);
+        textField.setBackground(Color.black);
+        textField.setCaretColor(Color.white);
+
+
+        jbtn.addActionListener(this);
+
+
+        this.add(jbtn);
+        this.add(textField);
         this.pack();
         this.setVisible(true);
     }
@@ -17,6 +35,9 @@ public class TextFields extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+    if(e.getSource()==jbtn)
+    {
+        System.out.println("welcome "+textField.getText());
+    }
     }
 }
