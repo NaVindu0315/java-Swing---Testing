147e193b703f667c4eb8efc011d6f420a40515b9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-20 11:08:51 +0530

    layered pane

--- src/panes/LayeredPanes.java
+++ src/panes/LayeredPanes.java
@@ -15,7 +15,28 @@ public class LayeredPanes extends JFrame {
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setBounds(0,0,500,500);
 
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.red);
+        label1.setBounds(50,50,200,200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.GREEN);
+        label2.setBounds(100,100,200,200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.BLUE);
+        label3.setBounds(150,150,200,200);
+
+
+
 
         this.add(layeredPane);
+        layeredPane.add(label1);
+        layeredPane.add(label2);
+        layeredPane.add(label3);
+
     }
 }
