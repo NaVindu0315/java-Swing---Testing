fab9fd8d64db0093a5e116253fabfa4984ee2108
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-20 12:45:03 +0530

    launch page button click

--- src/pages/LaunchPage.java
+++ src/pages/LaunchPage.java
@@ -1,4 +1,31 @@
 package pages;
 
-public class LaunchPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LaunchPage implements ActionListener {
+    JFrame frame = new JFrame();
+    JButton mybutton = new JButton("next page");
+    public LaunchPage(){
+
+        mybutton.setBounds(100,160,200,40);
+        mybutton.setFocusable(false);
+        mybutton.addActionListener(this);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        frame.add(mybutton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==mybutton)
+        {
+            NewWindow  myWindow = new NewWindow();
+        }
+    }
 }
