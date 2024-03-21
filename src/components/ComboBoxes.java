7b374b0b1567a4d47f18d1814b0ed8950b539816
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-21 20:01:21 +0530

    combobox

---
+++ src/components/ComboBoxes.java
@@ -0,0 +1,21 @@
+package components;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ComboBoxes extends JFrame implements ActionListener  {
+
+     JComboBox combbox;
+    ComboBoxes(){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
