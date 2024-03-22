9af586d15a40d25748d4015b0f9d30209aca6e39
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-22 14:04:40 +0530

    added icons for menubar

--- src/components/MenuButtons.java
+++ src/components/MenuButtons.java
@@ -3,6 +3,7 @@ package components;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class MenuButtons extends JFrame implements ActionListener {
 
@@ -10,14 +11,39 @@ public class MenuButtons extends JFrame implements ActionListener {
     JMenu filemenu =  new JMenu("Files");
     JMenu editmenu = new JMenu("Edit");
     JMenu helmenu = new JMenu("Help");
+
+    JMenuItem loaditem = new JMenuItem("Load");
+    JMenuItem saveitem = new JMenuItem("Save");
+
+    JMenuItem exititem = new JMenuItem("exit");
+    ImageIcon loadIcon;
+    ImageIcon exiticon;
+
     public MenuButtons(){
 
         menuBar = new JMenuBar();
+
+        filemenu.add(loaditem);
+        filemenu.add(saveitem);
+        filemenu.add(exititem);
+        loaditem.addActionListener(this);
+        saveitem.addActionListener(this);
+        exititem.addActionListener(this);
+
+
         menuBar.add(filemenu);
         menuBar.add(helmenu);
         menuBar.add(editmenu);
         this.setJMenuBar(menuBar);
+        //for keyboard shortcut
+        loaditem.setMnemonic(KeyEvent.VK_L); //for load
+        saveitem.setMnemonic(KeyEvent.VK_S); //save item
 
+        loadIcon = new ImageIcon("dudu.png");
+        exiticon = new ImageIcon("bubu.png");
+
+        loaditem.setIcon(loadIcon);
+        exititem.setIcon(exiticon);
 
 
 
@@ -30,6 +56,17 @@ public class MenuButtons extends JFrame implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+     if(e.getSource()==loaditem)
+     {
+         System.out.println("Paka");
+     }
+     else if (e.getSource()==exititem)
+        {
+            System.out.println("payya");
+        }
+        else if (e.getSource()==saveitem)
+     {
+         System.out.println("pnna umesh");
+     }
     }
 }
