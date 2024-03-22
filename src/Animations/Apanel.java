6fe2be0a0a33156b9df9546e956b0991a1372227
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-23 01:49:17 +0530

    animations finsihed

--- src/Animations/Apanel.java
+++ src/Animations/Apanel.java
@@ -2,11 +2,13 @@ package Animations;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Apanel extends JPanel {
+public class Apanel extends JPanel implements ActionListener {
 
-    final int PANEL_WIDTH = 500;
-    final int PANEL_HEIGHT = 500;
+    final int PANEL_WIDTH = 800;
+    final int PANEL_HEIGHT = 800;
 
     Image dudu;
     Image background;
@@ -18,9 +20,39 @@ public class Apanel extends JPanel {
     int y=0;
 
     Apanel(){
+        this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_WIDTH));
+        this.setBackground(Color.PINK);
+        dudu = new ImageIcon("mocha.PNG").getImage();
+        timer = new Timer(10,this);
+        timer.start();
+
 
 
 
     }
+    public  void paint (Graphics g)
+    {
+        super.paint(g);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.drawImage(dudu,x,y,null);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(x>=PANEL_WIDTH-dudu.getWidth(null) ||x<0)
+        {
+            xVelocity = xVelocity * -1;
+        }
+        x =x +xVelocity;
+
+        if (y>=PANEL_HEIGHT-dudu.getHeight(null)||y<0)
+        {
+            yVelocity = yVelocity * -1;
+        }
+        y = y+yVelocity;
+
+
+        repaint();
+
+    }
 }
