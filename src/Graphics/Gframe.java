563c9e005120d01777e6cde42a6abe75605151e5
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-22 23:57:42 +0530

    graphics

---
+++ src/Graphics/Gframe.java
@@ -0,0 +1,18 @@
+package Graphics;
+
+import javax.swing.*;
+
+public class Gframe extends JFrame {
+
+
+    Gframe(){
+
+
+
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
