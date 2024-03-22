47586111b9ed93b0e6c7ab32e76c58d1511fc0f1
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-22 19:01:17 +0530

    mouse lisnters

--- src/Listners/MouseListners.java
+++ src/Listners/MouseListners.java
@@ -1,10 +1,21 @@
 package Listners;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class MouseListners extends JFrame {
+public class MouseListners extends JFrame implements MouseListener {
+
+    JLabel lbl;
 
     public MouseListners(){
+        lbl = new JLabel();
+        lbl.setBounds(0,0,100,100);
+        lbl.setBackground(Color.red);
+        lbl.setOpaque(true);
+        this.add(lbl);
+        lbl.addMouseListener(this);
 
         this.setSize(500,500);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -14,4 +25,29 @@ public class MouseListners extends JFrame {
        // this.addKeyListener(this);
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+          //  System.out.println("Ponnaya");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //System.out.println("Paka ebuwa");
+        System.out.println("Payya  alluwa");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Payya Athariya");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
