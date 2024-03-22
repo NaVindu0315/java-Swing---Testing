5b56c97c1b504e2a3bfcbbdb38f75d1d72d5d324
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-22 12:43:35 +0530

    sliders

--- src/components/Sliders.java
+++ src/components/Sliders.java
@@ -3,6 +3,7 @@ package components;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class Sliders extends JFrame implements ChangeListener {
@@ -16,7 +17,26 @@ public class Sliders extends JFrame implements ChangeListener {
         this.setTitle("Title demo");
         panel = new JPanel();
         label = new JLabel();
-        slider = new JSlider(0,100);
+        slider = new JSlider(0,100,50); //first last and starting point
+        slider.setPreferredSize(new Dimension(400,200));
+        slider.setPaintTicks(true);
+        slider.setMinorTickSpacing(10);
+        slider.setPaintTrack(true);
+        slider.setMajorTickSpacing(10);
+        //to make the slider verical
+    slider.setOrientation(SwingConstants.VERTICAL);
+        slider.setPaintLabels(true);
+        slider.addChangeListener(this);
+
+
+        panel.add(label);
+       panel.add(slider);
+       this.add(panel);
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();
+        this.setVisible(true);
 
 
 
@@ -24,5 +44,6 @@ public class Sliders extends JFrame implements ChangeListener {
     @Override
     public void stateChanged(ChangeEvent e) {
 
+        label.setText("c = "+slider.getValue());
     }
 }
