fdcbc2a41e9a3c1df8a0a8c9e54dc5ae5bfe94d5
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-22 23:48:56 +0530

    keybindinfg

--- src/keybindings/Game.java
+++ src/keybindings/Game.java
@@ -25,13 +25,27 @@ public class Game {
         label.setBackground(Color.RED);
         label.setBounds(100,100,100,100);
         label.setOpaque(true);
-        frame.add(label);
-        frame.setVisible(true);
+
 
         upAction = new UpAction();
         downAction = new DownAction();
         leftAction = new LeftAction();
         rightAction = new RightAction();
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("UP"),"upAction");
+        label.getActionMap().put("upAction",upAction);
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("DOWN"),"downAction");
+        label.getActionMap().put("downAction",downAction);
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),"rightAction");
+        label.getActionMap().put("rightAction",rightAction);
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("LEFT"),"leftAction");
+        label.getActionMap().put("leftAction",leftAction);
+        frame.add(label);
+        frame.setVisible(true);
+
 
 
 
@@ -47,7 +61,7 @@ public class Game {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            label.setLocation(label.getX(),label.getY()+10);
         }
     }
 
@@ -55,6 +69,7 @@ public class Game {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            label.setLocation(label.getX()-10,label.getY());
 
         }
     }
@@ -63,7 +78,7 @@ public class Game {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            label.setLocation(label.getX()+10,label.getY());
         }
     }
 
