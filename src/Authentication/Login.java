0f0de5048807571cc1981ccee8c80fa6954b27a1
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-23 14:05:54 +0530

    login page

---
+++ src/Authentication/Login.java
@@ -0,0 +1,47 @@
+package Authentication;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+
+    JButton btnsignup;
+    JButton btnlogin;
+    JLabel lblid;
+
+    JLabel lblemail;
+
+    JLabel lblpassword;
+
+    JTextField txtid;
+
+    JTextField txtemail;
+
+
+    JTextField txtpassword;
+
+    JPanel panel;
+
+    int v_id;
+    String v_name;
+    String v_email;
+
+    String v_password;
+
+
+
+
+    JLabel signlbl;
+
+
+    Login(){
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
