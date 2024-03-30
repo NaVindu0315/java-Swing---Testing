77052ca841c69450677695f1cba9da9a9810a890
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-30 22:33:29 +0530

    vector

--- src/java3d/Vector.java
+++ src/java3d/Vector.java
@@ -1,13 +1,23 @@
-package java3d;
 
 public class Vector {
-
-    double x,y,z;
-    public Vector(double x,double y ,double z)
+    double x = 0, y = 0, z = 0;
+    public Vector(double x, double y, double z)
     {
-        this.x =x;
-        this.y =y;
-        this.z = z;
+        double length = Math.sqrt(x * x + y * y + z * z);
+        if(length>0)
+        {
+            this.x = x/length;
+            this.y = y/length;
+            this.z = z/length;
+        }
     }
 
+    Vector CrossProduct(Vector V)
+    {
+        Vector CrossVector = new Vector(
+                y * V.z - z * V.y,
+                z * V.x - x * V.z,
+                x * V.y - y * V.x);
+        return CrossVector;
+    }
 }
