71a7a38f943b4734e6de14db4488c648e4d51500
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-30 10:30:41 +0530

    oval

---
+++ src/java3d/Screen.java
@@ -0,0 +1,12 @@
+package java3d;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen extends JPanel {
+
+    public void paintComponent(Graphics g)
+    {
+        g.fillOval(10,10,500,500);
+    }
+}
--- src/java3d/home3d.java
+++ src/java3d/home3d.java
@@ -5,10 +5,16 @@ import java.awt.*;
 
 public class home3d extends JFrame {
 
+    Screen screen = new Screen();
 
     public home3d(){
-        this.setUndecorated(true);
-        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+
+        this.add(screen);
+
+
+        //this.setUndecorated(true);
+       // this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        this.setSize(800,700);
         this.setVisible(true);
     }
 }
