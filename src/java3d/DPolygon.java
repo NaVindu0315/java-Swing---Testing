c3d2b72bb654d83ab7719230446a3cbed2fc6cfa
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-30 22:53:05 +0530

    dpolygon

--- src/java3d/DPolygon.java
+++ src/java3d/DPolygon.java
@@ -21,34 +21,43 @@ public class DPolygon {
 
     void createPolygon()
     {
-        double total = 0;
-        double[] newX = new double[x.length];
-        double	[] newY = new double[x.length];
-
-        for(int i = 0; i < x.length; i++)
-        {
-            newX[i] = 500 + 50 * Calculator.CalculatePositionX(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
-            newY[i] = 500 + 50 * Calculator.CalculatePositionY(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
-            total += Calculator.t;
-        }
-
         poly = Screen.NumberOfPolygons;
-        Screen.DrawablePolygons[Screen.NumberOfPolygons] = new PolygonObject(newX, newY, c);
-        Screen.DrawablePolygons[poly].AvgDist = total / x.length;
+        Screen.DrawablePolygons[Screen.NumberOfPolygons] = new PolygonObject(new double[]{}, new double[]{}, c);
+        updatePolygon();
     }
 
     void updatePolygon()
     {
+        double dx = - 50 * Calculator.CalculatePositionX(Screen.ViewFrom, Screen.ViewTo, Screen.ViewTo[0], Screen.ViewTo[1], Screen.ViewTo[2]);
+        double dy = - 50 * Calculator.CalculatePositionY(Screen.ViewFrom, Screen.ViewTo, Screen.ViewTo[0], Screen.ViewTo[1], Screen.ViewTo[2]);
         double[] newX = new double[x.length];
-        double	[] newY = new double[x.length];
+        double[] newY = new double[x.length];
 
         for(int i = 0; i < x.length; i++)
         {
-            newX[i] = 500 + 50 * Calculator.CalculatePositionX(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
-            newY[i] = 500 + 50 * Calculator.CalculatePositionY(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
+            newX[i] = dx + DDDTutorial.ScreenSize.getWidth()/2 + 50 * Calculator.CalculatePositionX(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
+            newY[i] = dy + DDDTutorial.ScreenSize.getHeight()/2 + 50 * Calculator.CalculatePositionY(Screen.ViewFrom, Screen.ViewTo, x[i], y[i], z[i]);
         }
 
         Screen.DrawablePolygons[poly] = new PolygonObject(newX, newY, c);
+        Screen.DrawablePolygons[poly].AvgDist = GetDist();
         Screen.NumberOfPolygons --;
+    }
+
+    double GetDist()
+    {
+        double total = 0;
+        for(int i = 0; i < x.length; i++)
+            total += GetDistanceToP(i);
+        return total / x.length;
+    }
+
+    double GetDistanceToP(int i)
+    {
+
+        return Math.sqrt(
+                (Screen.ViewFrom[0] - x[i])*(Screen.ViewFrom[0] - x[i]) +
+                        (Screen.ViewFrom[1] - y[i])*(Screen.ViewFrom[1] - y[i]) +
+                        (Screen.ViewFrom[2] - z[i])*(Screen.ViewFrom[2] - z[i]));
     }
 }
